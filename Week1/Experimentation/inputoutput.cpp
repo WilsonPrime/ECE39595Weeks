95198c5985d8e518abdb429e37798dc754b299b1
Wilson Wong <wong368@eceprog5.ecn.purdue.edu>
2023-08-25 10:49:43 -0400

    Added a inputoutput program

---
+++ Week1/Experimentation/inputoutput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+
+using std::cout;
+using std::cin;
+
+int main(void){
+
+    int input_one,input_two;
+    cout << "Please insert an input for one";
+    cin >> input_one;
+
+    cout << "Please insert an input for two";
+    cin >> input_two;
+
+    cout << "Output: " << input_one + input_two;
+
+
+
+}
